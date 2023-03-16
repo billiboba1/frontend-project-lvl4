91c34cbd43f0a39269d28a7341889e63185d6fce
billiboba1 <shadrinalexander@mail.ru>
2023-03-16 13:35:30 +0300

    9

--- frontend/src/modules/chat/components/chat.jsx
+++ frontend/src/modules/chat/components/chat.jsx
@@ -6,7 +6,7 @@ import ChannelsComponent from './channelsComponent';
 export const chatComponent = () => {
   const navigate = useNavigate();
   return (
-    <div className="chat d-inline-flex justify-content-center m-3 container-xl bg-dark rounded-3">
+    <div className="chat h-75 d-inline-flex align-items-stretch container-xl justify-content-around m-3 bg-dark rounded-3">
       <div className="channels d-flex flex-column">
         <ChannelsComponent />
       </div>
